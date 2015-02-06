ed514c6ce30e84be68192a1d2c01d631ab521d34
kolesnikovakate <kolesnikovakate@gmail.com>
2015-02-06 10:55:05 +0300

    Создание проекта

---
+++ EightBall/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EightBall
+//
+//  Created by Екатерина Колесникова on 06.02.15.
+//  Copyright (c) 2015 KKate. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+xcodeproj 'EightBall.xcodeproj'
+platform :ios, '7.1'
+
+pod "MagicalRecord", :git => 'https://github.com/magicalpanda/MagicalRecord.git', :branch => 'develop'
